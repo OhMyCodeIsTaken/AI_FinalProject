7861a2695a48586551bae71c520ccac372347793
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-06-13 16:56:43 +0300

    Planets Rotation

--- Assets/Scripts/Planets/Planet.cs
+++ Assets/Scripts/Planets/Planet.cs
@@ -7,5 +7,19 @@ public class Planet : MonoBehaviour
 {
     [SerializeField] private MineralInventory mineralInventory;
 
+    Vector3 planetRotation = Vector3.up;
+
+    [SerializeField] private int rotationSpeed;
+
     public MineralInventory MineralInventory { get => mineralInventory; set => mineralInventory = value; }
+
+    protected void RotatePlanet()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void Update()
+    {
+        RotatePlanet();
+    }
 }
